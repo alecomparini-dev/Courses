a1fab1bf5530041e45301e914cb51a6a6829e11a
compa <alecomparini.dev@gmail.com>
2023-02-19 17:14:05 -0300

    binding of labels, inputs and button

--- BlackFront-LoginScreen/BlackFront-LoginScreen/ViewController.swift
+++ BlackFront-LoginScreen/BlackFront-LoginScreen/ViewController.swift
@@ -9,11 +9,27 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var welcomeLabel: UILabel!
+    @IBOutlet weak var avatarImageView: UIImageView!
+    @IBOutlet weak var emailLabel: UILabel!
+    @IBOutlet weak var passwordLabel: UILabel!
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var loginButton: UIButton!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    
+    @IBAction func pressedLoginButton(_ sender: UIButton) {
+        print("Button pressed")
+        
+    }
+    
+    
 
 }
 
