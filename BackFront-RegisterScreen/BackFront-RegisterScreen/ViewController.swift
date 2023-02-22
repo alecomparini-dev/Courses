df3f43cb96366a50bba1360f183c724030317143
compa <alecomparini.dev@gmail.com>
2023-02-22 16:38:18 -0300

    new project register screen

---
+++ BackFront-RegisterScreen/BackFront-RegisterScreen/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BackFront-RegisterScreen
+//
+//  Created by Alessandro Comparini on 22/02/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
