d5d6cf2fe3d5c414c7c1c53ee7e81006e35be30a
compa <alecomparini.dev@gmail.com>
2023-02-23 19:14:13 -0300

    new project navigation deprecated

---
+++ BackFront-NavigatorDeprecated/BackFront-NavigatorDeprecated/navigatorScreen2VC.swift
@@ -0,0 +1,29 @@
+//
+//  navigatorScreen2VC.swift
+//  BackFront-NavigatorDeprecated
+//
+//  Created by Alessandro Comparini on 23/02/23.
+//
+
+import UIKit
+
+class navigatorScreen2VC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
