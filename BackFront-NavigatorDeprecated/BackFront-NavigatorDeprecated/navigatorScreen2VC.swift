739bf234a1e71c6a2c7bccedb7c830f0b8262189
compa <alecomparini.dev@gmail.com>
2023-02-23 19:33:49 -0300

    finished

--- BackFront-NavigatorDeprecated/BackFront-NavigatorDeprecated/navigatorScreen2VC.swift
+++ BackFront-NavigatorDeprecated/BackFront-NavigatorDeprecated/navigatorScreen2VC.swift
@@ -11,19 +11,17 @@ class navigatorScreen2VC: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+    }
 
-        // Do any additional setup after loading the view.
+    
+    @IBAction func pressedCloseScreenButton(_ sender: UIButton) {
+        self.navigationController?.popViewController(animated: true)
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    
+    @IBAction func pressedReturnScreen1Button(_ sender: UIButton) {
+        self.navigationController?.popToRootViewController(animated: true)        
     }
-    */
+    
 
 }
