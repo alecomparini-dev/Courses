7b0948cf5cf7ee7859564c640b7eb5918315ceb3
compa <alecomparini.dev@gmail.com>
2023-02-25 16:26:32 -0300

    new project, collection view

---
+++ BackFront-CollectionView/BackFront-CollectionView/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BackFront-CollectionView
+//
+//  Created by Alessandro Comparini on 25/02/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    
+    
+
+}
+
