35db42a852a43379caf84eab6b31534658a0c816
compa <alecomparini.dev@gmail.com>
2023-03-02 11:55:00 -0300

    fix: wrong image name

--- BackFront-ViewCode-LoginScreen-BFNFT/BackFront-ViewCode-LoginScreen-BFNFT/Login/Screen/LoginScreen.swift
+++ BackFront-ViewCode-LoginScreen-BFNFT/BackFront-ViewCode-LoginScreen-BFNFT/Login/Screen/LoginScreen.swift
@@ -11,11 +11,37 @@ class LoginScreen: UIView {
 
     override init(frame: CGRect) {
         super.init(frame: frame)
-        backgroundColor = .red
+        addElements()
+        configConstraints()
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+    
+    lazy var backGroundImage: UIImageView = {
+        let img = UIImageView()
+        img.translatesAutoresizingMaskIntoConstraints = false
+        img.contentMode = .scaleAspectFill
+        img.image = UIImage(named: "BG Login")
+        return img
+    }()
+    
+    
+    
+    //MARK- Functions
+    private func addElements() {
+        addSubview(backGroundImage)
+    }
+    
+    
+    private func configConstraints() {
+        NSLayoutConstraint.activate([
+            backGroundImage.topAnchor.constraint(equalTo: topAnchor, constant: 0),
+            backGroundImage.bottomAnchor.constraint(equalTo: bottomAnchor, constant: 0),
+            backGroundImage.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 0),
+            backGroundImage.trailingAnchor.constraint(equalTo: trailingAnchor, constant: 0),
+        ])
     }
     
 }
