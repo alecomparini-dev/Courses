05732a5cc2b2abf9f11742e9ccbcbbc0c906b06e
compa <alecomparini.dev@gmail.com>
2023-03-08 20:16:16 -0300

    alamofire teste 1 finished

--- BackFront-Alamofire/BackFront-Alamofire/Service/HomeService.swift
+++ BackFront-Alamofire/BackFront-Alamofire/Service/HomeService.swift
@@ -6,9 +6,28 @@
 //
 
 import UIKit
+import Alamofire
 
 class HomeService {
 
+    private let URL: String = "https://run.mocky.io/v3/85ade36b-22a0-4a0d-9833-5818a3225521"
+    
+    public func getAllDataJsonAlamofire(completion: @escaping (HomeData?, Error?) -> Void) {
+        
+        AF.request(self.URL, method: .get)
+            .validate(statusCode: 200..<300)
+            .validate(contentType: ["application/json"])
+            .responseDecodable(of: HomeData.self) { response in
+                switch response.result {
+                case .success( let data):
+                    completion(data,nil)
+                case .failure( let error):
+                    completion(nil, error)
+                }
+            }
+        
+    }
+    
     public func getAllDataJsonFile(completion: @escaping (HomeData?, Error?) -> Void) {
         if let url = Bundle.main.url(forResource: "HomeData", withExtension: "json") {
             
